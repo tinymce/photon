2d74366b4a29b484ab1fa3d398d88bbff6bcc884
Marli Oshlack <marli.oshlack@ephox.com>
2012-10-26 05:35:05 -0700

    Fix OuterPosition.

--- src/main/js/ephox/photon/OuterPosition.js
+++ src/main/js/ephox/photon/OuterPosition.js
@@ -7,29 +7,29 @@ define(
     'ephox.photon.Frames',
     'ephox.sugar.alien.Position',
     'ephox.sugar.api.Element',
-    'ephox.sugar.api.Height',
     'ephox.sugar.api.Location',
     'ephox.sugar.api.Scroll',
     'global!document'
   ],
 
-  function (Arr, Fun, Frames, Position, Element, Height, Location, Scroll, document) {
+  function (Arr, Fun, Frames, Position, Element, Location, Scroll, document) {
     var find = function (element) {
       var doc = Element.fromDom(document);
       var scroll = Scroll.get(doc);
-      var offset = Location.absolute(element);
       var path = Frames.pathTo(element, doc);
 
-      return path.fold(Fun.constant(offset), function (v) {
-        var r = Arr.foldr(v, function (b, a) {
-          var loc = Location.absolute(a);
+      return path.fold(Fun.curry(Location.absolute, element), function (frames) {
+        var offset = Location.viewport(element);
+
+        var r = Arr.foldr(frames, function (b, a) {
+          var loc = Location.viewport(a);
           return {
             left: b.left + loc.left(),
             top: b.top + loc.top()
           };
         }, { left: 0, top: 0 });
 
-        return Position(r.left + offset.left() - scroll.left(), r.top + offset.top() + Height.getOuter(element) - scroll.top());
+        return Position(r.left + offset.left() + scroll.left(), r.top + offset.top() + scroll.top());
       });
     };
 
