75c217c873a53134123a68d0209464fc9413f387
ephox_dev <dev@klaatu.(none)>
2013-03-01 19:00:25 +1000

    walking up

--- src/main/js/ephox/photon/Frames.js
+++ src/main/js/ephox/photon/Frames.js
@@ -7,11 +7,12 @@ define(
     'ephox.perhaps.Option',
     'ephox.photon.Reader',
     'ephox.sugar.api.Compare',
+    'ephox.sugar.api.Element',
     'ephox.sugar.api.SelectorFilter',
     'ephox.sugar.api.Traverse'
   ],
 
-  function (Arr, Fun, Option, Reader, Compare, SelectorFilter, Traverse) {
+  function (Arr, Fun, Option, Reader, Compare, Element, SelectorFilter, Traverse) {
 
     var curry = Fun.curry;
 
@@ -37,8 +38,38 @@ define(
       return pather(doc, [], pred);
     };
 
+
+    // At each stage:
+     // if document is not an iframe element -> return []
+     // if document is an iframe element -> return [iframe-document]:[rest from iframe-document]
+
+    var getFrameTag = function (doc) {
+      var frame = doc.dom().defaultView.frameElement;
+      if (frame !== null && frame !== undefined) {
+        var element = Element.fromDom(frame);
+        return Option.some(element);
+      } else {
+        return Option.none();
+      }
+    };
+
+    var walkUp = function (doc) {
+      var frame = getFrameTag(doc);
+      return frame.fold(Fun.constant([]), function (f) {
+        var parent = Traverse.owner(f);
+        var rest = walkUp(parent);
+        return [f].concat(rest);
+      });
+    };
+
+    var palax = function (element, doc) {
+      var d = Traverse.owner(element);
+      var paths = walkUp(d);
+      return Option.some(paths);
+    };
+
     return {
-      pathTo: pathTo
+      pathTo: palax
     };
   }
 );
