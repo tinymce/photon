4d02f88e22b26e9e1b1710f2e355e670e91c2129
Andrew Herron <thespyder@programmer.net>
2014-06-30 17:06:55 +1000

    TBIO-1017: Cap navigation at the bolt script document. Rework demo to show this.

--- src/main/js/ephox/photon/Navigation.js
+++ src/main/js/ephox/photon/Navigation.js
@@ -4,12 +4,14 @@ define(
   [
     'ephox.perhaps.Option',
     'ephox.sugar.api.Element',
-    'ephox.sugar.api.Traverse'
+    'ephox.sugar.api.Traverse',
+    'global!document'
   ],
 
-  function (Option, Element, Traverse) {
+  function (Option, Element, Traverse, document) {
     var view = function (doc) {
-      var frame = doc.dom().defaultView.frameElement;
+      // If the editor is in an iframe, don't walk up to the parent window
+      var frame = doc.dom() === document ? null : doc.dom().defaultView.frameElement;
       if (frame !== null && frame !== undefined) {
         var element = Element.fromDom(frame);
         return Option.some(element);
