be6cd4619438f65d6214598adb70eda271c260e8
Andrew Herron <thespyder@programmer.net>
2015-08-05 10:39:33 +1000

    TBIO-2989: Throw a useful error message instead of 'open is not a function'

--- src/main/js/ephox/photon/Writer.js
+++ src/main/js/ephox/photon/Writer.js
@@ -2,12 +2,16 @@ define(
   'ephox.photon.Writer',
 
   [
-    'ephox.photon.Reader'
+    'ephox.photon.Reader',
+    'ephox.sugar.api.Traverse'
   ],
 
-  function (Reader) {
+  function (Reader, Traverse) {
 
     var write = function (element, content) {
+      var hasParent = Traverse.parent(element).isSome();
+      if (!hasParent) throw 'Internal error: attempted to write to an iframe that is not in the DOM';
+
       var doc = Reader.doc(element);
       var dom = doc.dom();
       dom.open();
