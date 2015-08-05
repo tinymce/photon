c5223642de6837bc98e5d300004b3a93ab6c5750
Andrew Herron <thespyder@programmer.net>
2015-08-06 09:51:06 +1000

    Check Body.inBody() instead of just has parent

--- src/main/js/ephox/photon/Writer.js
+++ src/main/js/ephox/photon/Writer.js
@@ -3,14 +3,13 @@ define(
 
   [
     'ephox.photon.Reader',
-    'ephox.sugar.api.Traverse'
+    'ephox.sugar.api.Body'
   ],
 
-  function (Reader, Traverse) {
+  function (Reader, Body) {
 
     var write = function (element, content) {
-      var hasParent = Traverse.parent(element).isSome();
-      if (!hasParent) throw 'Internal error: attempted to write to an iframe that is not in the DOM';
+      if (!Body.inBody(element)) throw 'Internal error: attempted to write to an iframe that is not in the DOM';
 
       var doc = Reader.doc(element);
       var dom = doc.dom();
