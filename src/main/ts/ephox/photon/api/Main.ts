3cfeb00a1dc36d2b94262f18aaab5d7ab455a314
Spocke <spocke@moxiecode.com>
2017-12-11 16:57:14 +0100

    TINY-1461: added main api entry point

---
+++ src/main/ts/ephox/photon/api/Main.ts
@@ -0,0 +1,15 @@
+import Frames from './../Frames';
+import Navigation from './../Navigation';
+import OuterPosition from './../OuterPosition';
+import Reader from './../Reader';
+import Sandbox from './../Sandbox';
+import Writer from './../Writer';
+
+export {
+  Frames,
+  Navigation,
+  OuterPosition,
+  Reader,
+  Sandbox,
+  Writer
+};
